188ceb01e27c967f502126b0d2f978293b2c1c22
Kukuh Alfian <hi@kalfian.com>
2021-06-26 04:42:43 +0700

    [ADD] ViewBinding & Awesome Toast 😎

--- app/src/main/java/com/kalfian/infokosan/LoginActivity.kt
+++ app/src/main/java/com/kalfian/infokosan/LoginActivity.kt
@@ -2,10 +2,33 @@ package com.kalfian.infokosan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.kalfian.infokosan.databinding.ActivityLoginBinding
+import www.sanju.motiontoast.MotionToast
 
 class LoginActivity : AppCompatActivity() {
+
+    // Like the XML name activity_login to ActivityLoginBinding
+    private lateinit var b: ActivityLoginBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        b = ActivityLoginBinding.inflate(layoutInflater)
+        val v = b.root
+        setContentView(v)
+
+        b.loginBtn.setOnClickListener {
+            MotionToast.createColorToast(this,"Login success!",
+                "Success Login",
+                MotionToast.TOAST_SUCCESS,
+                MotionToast.GRAVITY_BOTTOM,
+                MotionToast.LONG_DURATION,
+                ResourcesCompat.getFont(this,R.font.helvetica_regular)
+            )
+        }
+
+
+
     }
 }
