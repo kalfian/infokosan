35f36b25b6b9311ed8334aef9eb65f1b7969e250
Kukuh Alfian <hI@kalfian.com>
2021-07-15 00:47:11 +0700

    [ADD] Detail Property Layout

--- app/src/main/java/com/kalfian/infokosan/modules/property/DetailPropertyActivity.kt
+++ app/src/main/java/com/kalfian/infokosan/modules/property/DetailPropertyActivity.kt
@@ -1,15 +1,23 @@
 package com.kalfian.infokosan.modules.property
 
-import android.graphics.Color.GRAY
-import android.graphics.Color.WHITE
+import android.R.style
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.kalfian.infokosan.R
 import com.kalfian.infokosan.adapters.SliderAdapter
 import com.kalfian.infokosan.databinding.ActivityDetailPropertyBinding
 import com.kalfian.infokosan.models.sliders.SliderItem
 import com.kalfian.infokosan.utils.Constant
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.annotation.Symbol
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolManager
+import com.mapbox.mapboxsdk.plugins.annotation.SymbolOptions
 import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType
 import com.smarteist.autoimageslider.SliderAnimations
 import com.smarteist.autoimageslider.SliderView
@@ -22,6 +30,8 @@ class DetailPropertyActivity : AppCompatActivity() {
     private lateinit var adapter: SliderAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
         super.onCreate(savedInstanceState)
         b = ActivityDetailPropertyBinding.inflate(layoutInflater)
         setContentView(b.root)
@@ -34,7 +44,39 @@ class DetailPropertyActivity : AppCompatActivity() {
 
         settingSlider()
         addItemSlider()
+        addMap(savedInstanceState)
 
+    }
+
+    private fun addMap(savedInstanceState: Bundle?) {
+        b.mapKos.onCreate(savedInstanceState)
+        b.mapKos.getMapAsync { mapboxMap ->
+
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+
+                val symbolManager = SymbolManager(b.mapKos, mapboxMap, it)
+                symbolManager.iconAllowOverlap = true
+                ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_place_24, null)?.let { imgRes ->
+                    it.addImage("myMarker",
+                        imgRes
+                    )
+                }
+                symbolManager.create(
+                    SymbolOptions()
+                    .withLatLng(LatLng(-7.9696, 112.6160))
+                    .withIconImage("myMarker")
+                )
+            }
+
+            val position = CameraPosition.Builder()
+                .target(LatLng(-7.9696, 112.6160))
+                .zoom(18.0)
+                .tilt(20.0)
+                .build()
+            mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 1000)
+
+
+        }
     }
 
     private fun settingSlider() {
@@ -59,5 +101,40 @@ class DetailPropertyActivity : AppCompatActivity() {
             sliderItemList.add(sliderItem)
         }
         adapter.renewItems(sliderItemList)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        b.mapKos.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        b.mapKos.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        b.mapKos.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        b.mapKos.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        b.mapKos.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        b.mapKos.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        b.mapKos.onDestroy()
     }
 }
