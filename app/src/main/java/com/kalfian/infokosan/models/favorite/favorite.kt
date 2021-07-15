5ef0f96c2649fa79924755e7283a1d4b27180486
Kukuh Alfian <hI@kalfian.com>
2021-07-15 21:55:56 +0700

    [ADD] Room

---
+++ app/src/main/java/com/kalfian/infokosan/models/favorite/favorite.kt
@@ -0,0 +1,16 @@
+package com.kalfian.infokosan.models.favorite
+
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "favorite")
+//Parcelable annotation to make parcelable object
+@Parcelize
+data class Favorite(
+    @PrimaryKey(autoGenerate = false)@ColumnInfo(name = "id") var id: Int = 0,
+    @ColumnInfo(name = "title") var title: String = "",
+    @ColumnInfo(name = "body") var body: String = ""
+) : Parcelable
